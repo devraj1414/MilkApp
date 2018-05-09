685a499d8b4deaa6715b1d7f0bc78398f2817517
Suruchi Kumari <ruchiraj1604@gmail.com>
2018-05-09 08:31:18 +0530

    Initial Commit

---
+++ MilkApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MilkApp
+//
+//  Created by Suruchi Kumari on 09/05/18.
+//  Copyright © 2018 Suruchi Kumari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
